d5ce28fe76d24a908a877e92bcd5b90ec7b24aed
Oliver Lade <oliver@urbanetic.net>
2015-10-27 07:16:11 +0000

    Improved run script to exit on failure.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-python $GEOSERVER_HOME/docker/setup.py
-
-echo "Starting GeoServer..."
-$GEOSERVER_HOME/bin/startup.sh
+python $GEOSERVER_HOME/docker/setup.py \
+    && echo "Starting GeoServer..." \
+    && $GEOSERVER_HOME/bin/startup.sh
